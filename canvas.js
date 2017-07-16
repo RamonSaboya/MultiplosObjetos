ffb54c5f5655beefea2b55f2841bf23f9112bbed
hcaula <henrique.caula@hotmail.com>
2017-07-16 11:58:07 -0300

    Set canvas height and width to match div 'stage'

---
+++ canvas.js
@@ -0,0 +1,13 @@
+var c = document.getElementById("myCanvas");
+
+/*
+ * Set canvas height to match div stage
+*/
+c.height = document.getElementById("canvas").clientHeight;
+c.width = document.getElementById("canvas").clientWidth;
+
+var ctx = c.getContext("2d");
+
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
