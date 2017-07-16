7f1fab3170bdfd80965a0147fff2b25083203eb0
hcaula <henrique.caula@hotmail.com>
2017-07-16 16:12:19 -0300

    Ilumination and camera files are stored

--- files.js
+++ files.js
@@ -1,6 +1,6 @@
-var objects = [];
-var ilumination;
-var camera;
+var objectsFiles = [];
+var iluminationFile;
+var cameraFile;
 var txtObj = '';
 var txtCam = '';
 var txtIlu = '';
@@ -12,21 +12,28 @@ var addFile = function(param) {
 
   for(i = 0; i < files.length; i++) {
     reader.readAsBinaryString(files[i]);
+    var txt = '';
+
     if(param == "objs"){
-      objects.push(reader.result);
+      objectsFiles.push(files[i]);
       if (objects.length > 1) txtObj += " | "
       txtObj += files[i].name;
+      reader.onloadend = function(event){
+        objectsFiles.push(reader.result);
+      }
       document.getElementById("objects").innerHTML = "<p id='objects'>" + txtObj + "</p>";
     } else if (param == "cam") {
       txtCam = files[i].name;
-      camera = reader.result;
+      reader.onloadend = function(event){
+        cameraFile = reader.result;
+      }
       document.getElementById("camera").innerHTML = "<p id='objects'>" + txtCam + "</p>";
     } else {
       txtIlu = files[i].name;
-      ilumination = reader.result;
+      reader.onloadend = function(event){
+        iluminationFile = reader.result;
+      }
       document.getElementById("ilumination").innerHTML = "<p id='objects'>" + txtIlu + "</p>";
     }
-
   }
-
 }
