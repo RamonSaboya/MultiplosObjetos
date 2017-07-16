920906b7c46355394bef45a1791c328ee7c2a57f
hcaula <henrique.caula@hotmail.com>
2017-07-16 15:36:52 -0300

    Camera and ilumination files

--- files.js
+++ files.js
@@ -1,30 +1,30 @@
 var objects = [];
-var ilumination = [];
-var camera = [];
+var ilumination;
+var camera;
 var txtObj = '';
 var txtCam = '';
 var txtIlu = '';
 
 var reader = new FileReader();
 
-var addObjects = function() {
-  var filesDOM = document.getElementById("objs");
-  var files = filesDOM.files;
-  var id = filesDOM.id;
+var addFile = function(param) {
+  var files = document.getElementById(param).files;
 
   for(i = 0; i < files.length; i++) {
     reader.readAsBinaryString(files[i]);
-    if(id == "objs"){
+    if(param == "objs"){
       objects.push(reader.result);
-      if (objects.length > 1){
-        txtObj += " | "
-      }
+      if (objects.length > 1) txtObj += " | "
       txtObj += files[i].name;
       document.getElementById("objects").innerHTML = "<p id='objects'>" + txtObj + "</p>";
-    } else if (id == "ilum") {
-      
+    } else if (param == "cam") {
+      txtCam = files[i].name;
+      camera = reader.result;
+      document.getElementById("camera").innerHTML = "<p id='objects'>" + txtCam + "</p>";
     } else {
-
+      txtIlu = files[i].name;
+      ilumination = reader.result;
+      document.getElementById("ilumination").innerHTML = "<p id='objects'>" + txtIlu + "</p>";
     }
 
   }
