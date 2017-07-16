2153eaa2edc5d2d491e83c4f31fc0d85dc51a765
hcaula <henrique.caula@hotmail.com>
2017-07-16 12:46:44 -0300

    Add file can read now

--- canvas.js
+++ canvas.js
@@ -1,4 +1,5 @@
 var c = document.getElementById("myCanvas");
+var reader = new FileReader();
 
 /* Set canvas height and width to match div stage */
 c.height = document.getElementById("canvas").clientHeight;
@@ -6,14 +7,15 @@ c.width = document.getElementById("canvas").clientWidth;
 
 var ctx = c.getContext("2d");
 
-
 var addFile = function() {
   var x = document.getElementById("myFile");
-  var txt = "";
+  var file;
   if ('files' in x) {
-
+    for (var i = 0; i < x.files.length; i++) {
+      file = x.files[i];
+    }
   }
-
+  reader.readAsBinaryString(file);
 }
 
 ctx.moveTo(0,0);
