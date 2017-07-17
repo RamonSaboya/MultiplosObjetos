11cb398f208e20c6c5f01331003b04fbec31dee9
hcaula <henrique.caula@hotmail.com>
2017-07-17 14:27:32 -0300

    Triangles stored in array

--- js/Application/objects.js
+++ js/Application/objects.js
@@ -3,12 +3,13 @@ var triangles = [];
 
 var startObject = function(file, order) {
   points[order] = [];
+  triangles[order] = [];
   var qtds = file.split("\n")[0];
   var qtdPoints = parseInt(qtds.split(" ")[0]);
   var qtdTriangles = parseInt(qtds.split(" ")[1]);
 
   var line, x, y, z, point, triangle;
-  for(i = 1; i < qtdPoints+qtdTriangles; i++){
+  for(i = 1; i < qtdPoints+qtdTriangles+1; i++){
     line = file.split("\n")[i];
     x = line.split(" ")[0];
     y = line.split(" ")[1];
@@ -17,11 +18,10 @@ var startObject = function(file, order) {
       point = new Point3D(x, y, z);
       points[order].push(point);
     } else {
-      // triangle = new Triangle(x, y, z);
-      // triangles[order].push(triangle);
+      triangle = new Triangle(x, y, z);
+      triangles[order].push(triangle);
     }
   }
-  console.log(points);
 }
 
 var test = function(){
